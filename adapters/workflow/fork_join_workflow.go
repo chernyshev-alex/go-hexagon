7c6722fd0e8e61b92c26e08f3a4f491e587e84b1
chernyshev-alexander <achernyshev@griddynamics.com>
2023-06-12 22:51:50 +0200

    fork-join 2

--- adapters/workflow/fork_join_workflow.go
+++ adapters/workflow/fork_join_workflow.go
@@ -10,40 +10,46 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
+type Action string
+type Branches map[string]Action
+type DagDefinition map[string]Branches
+
 type ForkJoinWorkflow struct {
-	wkName string
-	config map[string]interface{}
+	name string
+	dag  DagDefinition
 }
 
-func NewForkJoinWorkflow(name string, config map[string]interface{}) *ForkJoinWorkflow {
-	return &ForkJoinWorkflow{wkName: name, config: config}
+func NewForkJoinWorkflow(name string, dag DagDefinition) *ForkJoinWorkflow {
+	return &ForkJoinWorkflow{name: name, dag: dag}
 }
 
-func (p ForkJoinWorkflow) queueName() string { return p.wkName + "_queue" }
+func (p ForkJoinWorkflow) queueName() string { return p.name + "_queue" }
 
 func (p ForkJoinWorkflow) RunEtlWorkFlow() error {
 	cli, _ := client.Dial(client.Options{})
 	defer cli.Close()
 
 	opts := client.StartWorkflowOptions{
-		ID:        fmt.Sprintf("etl-%s-%s", p.wkName, uuid.NewString()),
+		ID:        fmt.Sprintf("etl-%s-%s", p.name, uuid.NewString()),
 		TaskQueue: p.queueName(),
 	}
 
 	var results []string
-
-	workflowRun, _ := cli.ExecuteWorkflow(context.Background(), opts, p.ParentWorkflow)
-	if err := workflowRun.Get(context.Background(), &results); err != nil {
-		return err
+	for _, v := range p.dag {
+		workflowRun, _ := cli.ExecuteWorkflow(context.Background(), opts, p.ParentWorkflow, v)
+		if err := workflowRun.Get(context.Background(), &results); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-func (p ForkJoinWorkflow) ParentWorkflow(ctx workflow.Context) ([]string, error) {
+func (p ForkJoinWorkflow) ParentWorkflow(ctx workflow.Context, branches Branches) ([]string, error) {
 	futures := []workflow.Future{}
-	for i := 0; i < 5; i++ {
+	for k, v := range branches {
+		//for i := 0; i < 5; i++ {
 		childCtx := workflow.WithChildOptions(ctx, workflow.ChildWorkflowOptions{})
-		future := workflow.ExecuteChildWorkflow(childCtx, p.TaskChildWorkflow, fmt.Sprintf("Task-%d", i), p.config)
+		future := workflow.ExecuteChildWorkflow(childCtx, p.TaskChildWorkflow, fmt.Sprintf("Task-%s", k), v)
 		futures = append(futures, future)
 	}
 	logger := workflow.GetLogger(ctx)
@@ -58,14 +64,13 @@ func (p ForkJoinWorkflow) ParentWorkflow(ctx workflow.Context) ([]string, error)
 	return results, nil
 }
 
-func (p ForkJoinWorkflow) TaskChildWorkflow(ctx workflow.Context, taskID string, config map[string]interface{}) (string, error) {
+func (p ForkJoinWorkflow) TaskChildWorkflow(ctx workflow.Context, taskID string, action string) (string, error) {
 	opts := workflow.ActivityOptions{StartToCloseTimeout: 10 * time.Second}
 	ctx1 := workflow.WithActivityOptions(ctx, opts)
 	logger := workflow.GetLogger(ctx1)
 
 	var result string
-	var sql = "insert into .." //  get SQL to execute
-	if err := workflow.ExecuteActivity(ctx1, p.RunSQL, taskID, sql).Get(ctx1, &result); err != nil {
+	if err := workflow.ExecuteActivity(ctx1, p.RunSQL, taskID, action).Get(ctx1, &result); err != nil {
 		logger.Error("failed", err)
 		return "", err
 	}
@@ -74,7 +79,7 @@ func (p ForkJoinWorkflow) TaskChildWorkflow(ctx workflow.Context, taskID string,
 
 var task3_count = 3
 
-func (p *ForkJoinWorkflow) RunSQL(ctx context.Context, taskID string, sqlStatement string) (string, error) {
+func (p *ForkJoinWorkflow) RunSQL(ctx context.Context, taskID string, action string) (string, error) {
 	// connect to db and run SQL statement
 	if taskID == "Task-3" && task3_count > 0 {
 		task3_count--
--- adapters/workflow/fork_join_workflow_test.go
+++ adapters/workflow/fork_join_workflow_test.go
@@ -9,7 +9,22 @@ import (
 )
 
 func TestForkJoinWorkflow(t *testing.T) {
-	w := NewForkJoinWorkflow("INGEST_W22K", map[string]interface{}{})
+
+	dag := DagDefinition{
+		"fork-1": {
+			"branch-1": "action-1",
+			"branch-2": "action-2",
+			"branch-3": "action-3",
+			"branch-4": "action-4",
+		},
+		"fork-2": {
+			"branch-5": "action-5",
+			"branch-6": "action-6",
+			"branch-7": "action-7",
+		},
+	}
+
+	w := NewForkJoinWorkflow("INGEST_W22K", dag)
 	if err := w.RunEtlWorkFlow(); err != nil {
 		t.Fatalf("failed %s", err)
 	}
@@ -21,7 +36,7 @@ func TestForkJoinWorker(t *testing.T) {
 	cli, _ := client.Dial(client.Options{})
 	defer cli.Close()
 
-	workFlow := NewForkJoinWorkflow("INGEST_W22K", map[string]interface{}{})
+	workFlow := NewForkJoinWorkflow("INGEST_W22K", nil)
 	// register worker's activities
 	instance := worker.New(cli, workFlow.queueName(), worker.Options{})
 	instance.RegisterWorkflow(workFlow.ParentWorkflow)
